74f582a00f381ac3c050fe90d2dc04b65a9acf5c
Viktor <Viktor@DESKTOP-MPG9A87>
2022-04-24 21:23:08 +0300

    Add project files.

---
+++ GameStore/Pages/Listing.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using GameStore.Models.Repository;
+using GameStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameStore.Pages
+{
+    public partial class Listing : System.Web.UI.Page
+    {
+        readonly private Repository repository = new Repository();
+        private int pageSize = 4;
+
+        protected int CurrentPage
+        {
+            get
+            {
+                int page;
+                page = int.TryParse(Request.QueryString["page"], out page) ? page : 1;
+                return page > MaxPage ? MaxPage : page;
+
+            }
+        }
+
+        protected int MaxPage
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)repository.Games.Count() / pageSize);
+            }
+        }
+
+        protected IEnumerable<Game> GetGames()
+        {
+            return repository.Games
+                   .OrderBy(g => g.GameId)
+                   .Skip((CurrentPage - 1) * pageSize)
+                   .Take(pageSize);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
